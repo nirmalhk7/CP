cbb1e953b0f9c89e59fcb88f77774478ccfcab8e
Nirmal Khedkar <nirmalhk7@gmail.com>
2020-07-13 09:11:51 +0530

    Status Update

---
+++ reverse-bits.py
@@ -0,0 +1,9 @@
+def soln(A):
+    A=str(A)
+    Aarr="0"*(32-len(A))+A
+    Aarr=[i for i in Aarr]
+    Aarr.reverse()
+    return int("".join(Aarr),2)
+if __name__ == "__main__":
+    print(soln('00000010100101000001111010011100'))
+    print("00111001011110000010100101000000")
